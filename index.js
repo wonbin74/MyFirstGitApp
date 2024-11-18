9188b8e37377d4e13a3a53c83f3922365ed40928
wonbin74 <wbrho92@gmail.com>
2024-11-18 15:49:25 +0900

    hotfix login 함수 username 체크

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //index.html 을 위한 js file
 function login(username, password) {
-    console.log(username + ' ' + password);
+    if (username) {
+        console.log(username + ' ' + password);
+    }
 };
 
 function logout() {
