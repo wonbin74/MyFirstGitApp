46016286f17ce027a21f67a0750d1a17617102d4
wonbin <wbrho92@gmail.com>
2024-11-18 14:15:06 +0900

    index.js login 함수 수정

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //index.html 을 위한 js file
-function login(username) {
-    console.log(username);
+function login(username, password) {
+    console.log(username + ' ' + password);
 };
 
 function logout() {
